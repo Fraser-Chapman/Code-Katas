7f0fd8daf7996ce638511a6ec0e6b495b53e4eca
fraser-chapman <fraser.chapman@autotrader.co.uk>
2018-11-22 09:17:45 +0000

    Completed challenge "Write a Java program to iterate through all elements in a array list."
    Signed-off-by: fraser-chapman <fraser.chapman@autotrader.co.uk>

---
+++ src/main/java/uk/co/autotrader/randomchallenges/IterateThroughArrayList.java
@@ -0,0 +1,33 @@
+package uk.co.autotrader.randomchallenges;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IterateThroughArrayList {
+
+    //Write a Java program to iterate through all elements in a array list.
+
+    public static void main(String[] args) {
+        List<Integer> timesTable = new ArrayList<>();
+        timesTable.add(1);
+        timesTable.add(2);
+        timesTable.add(3);
+        timesTable.add(4);
+        timesTable.add(5);
+        timesTable.add(6);
+        timesTable.add(7);
+        timesTable.add(8);
+        timesTable.add(9);
+        timesTable.add(10);
+
+        System.out.println("The two times table is:");
+        for(int number : timesTable){
+            System.out.println(number * 2);
+        }
+
+        System.out.println("The three times table is:");
+        for(int number : timesTable){
+            System.out.println(number * 3);
+        }
+    }
+}
