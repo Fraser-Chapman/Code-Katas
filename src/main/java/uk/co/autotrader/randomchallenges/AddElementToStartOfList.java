07d84bdb60066aae74054f5cdcd85d8f008acbf5
fraser-chapman <fraser.chapman@autotrader.co.uk>
2018-11-22 09:23:34 +0000

    Completed challenge "add en element to the start of an array list"
    Signed-off-by: fraser-chapman <fraser.chapman@autotrader.co.uk>

---
+++ src/main/java/uk/co/autotrader/randomchallenges/AddElementToStartOfList.java
@@ -0,0 +1,23 @@
+package uk.co.autotrader.randomchallenges;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddElementToStartOfList {
+    //Write a Java program to insert an element into the array list at the first position.
+
+    public static void main(String[] args) {
+
+        List<String> sentence = new ArrayList<>();
+        sentence.add("is");
+        sentence.add("a");
+        sentence.add("sentence.");
+
+        System.out.println(sentence.toString());
+        System.out.println("oops");
+
+        sentence.add(0, "This");
+
+        System.out.println(sentence.toString());
+    }
+}
