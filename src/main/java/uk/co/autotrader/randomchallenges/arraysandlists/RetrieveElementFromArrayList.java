b549bac051a399681451f607be99dd93ac2bf235
fraser-chapman <fraser.chapman@autotrader.co.uk>
2018-11-22 09:32:47 +0000

    Completed challenge "Write a Java program to retrieve an element (at a specified index) from a given array list."
    Signed-off-by: fraser-chapman <fraser.chapman@autotrader.co.uk>

--- src/main/java/uk/co/autotrader/randomchallenges/arraysandlists/RetrieveElementFromArrayList.java
+++ src/main/java/uk/co/autotrader/randomchallenges/arraysandlists/RetrieveElementFromArrayList.java
@@ -1,4 +1,18 @@
 package uk.co.autotrader.randomchallenges.arraysandlists;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RetrieveElementFromArrayList {
+
+    //Write a Java program to retrieve an element (at a specified index) from a given array list.
+
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Jack");
+        names.add("Jill");
+        names.add("Kevin");
+
+        System.out.println(names.get(1));
+    }
 }
