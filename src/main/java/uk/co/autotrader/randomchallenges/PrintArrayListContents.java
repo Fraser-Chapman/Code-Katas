7b64783f95b1ff063210bde7696045a4e774538f
Fraser-Chapman <fraserchapman16@gmail.com>
2018-11-22 09:08:34 +0000

    Completed challenge "Write a Java program to create a new array list, add some colors (string) and print out the collection"
    Signed-off-by: fraser-chapman <fraser.chapman@autotrader.co.uk>

---
+++ src/main/java/uk/co/autotrader/randomchallenges/PrintArrayListContents.java
@@ -0,0 +1,22 @@
+package uk.co.autotrader.randomchallenges;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintArrayListContents {
+    public static void main(String[] args) {
+        //Write a Java program to create a new array list,
+        //add some colors (string) and print out the collection.
+
+        List<String> colours = new ArrayList<>();
+
+        colours.add("Red");
+        colours.add("Blue");
+        colours.add("Green");
+        colours.add("Yellow");
+
+        for(String colour : colours) {
+            System.out.println(colour);
+        }
+    }
+}
