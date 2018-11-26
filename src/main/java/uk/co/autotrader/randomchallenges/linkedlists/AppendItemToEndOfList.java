adf45f47b731dfd7bef577ffbdc6e6d51a0e19a7
Fraser-Chapman <fraserchapman16@gmail.com>
2018-11-26 10:44:01 +0000

    Completed challenge "Write a Java program to append the specified element to the end of a linked list."
    Signed-off-by: fraser-chapman <fraser.chapman@autotrader.co.uk>

---
+++ src/main/java/uk/co/autotrader/randomchallenges/linkedlists/AppendItemToEndOfList.java
@@ -0,0 +1,13 @@
+package uk.co.autotrader.randomchallenges.linkedlists;
+
+import java.util.LinkedList;
+
+public class AppendItemToEndOfList {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        linkedList.add(1);
+
+        System.out.println(linkedList);
+    }
+}
