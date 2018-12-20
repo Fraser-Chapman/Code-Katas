3dc9ff6518cf553a72d2aa5c21c5daed2a8d0e6c
fraser-chapman <fraser.chapman@autotrader.co.uk>
2018-12-20 16:29:55 +0000

    added test to create empty grid and made it pass

--- src/main/java/uk/co/autotrader/randomchallenges/naughtsandcrosses/NaughtsAndCrosses.java
+++ src/main/java/uk/co/autotrader/randomchallenges/naughtsandcrosses/NaughtsAndCrosses.java
@@ -1,5 +1,10 @@
 package uk.co.autotrader.randomchallenges.naughtsandcrosses;
 
 public class NaughtsAndCrosses {
+    public String[][] grid;
+
+    public void generateGrid() {
+        grid = new String[][]{{"-", "-", "-"}, {"-", "-", "-"}, {"-", "-", "-"}};
+    }
 
 }
--- src/test/java/uk/co/autotrader/randomchallenges/naughtsandcrosses/NaughtsAndCrossesTest.java
+++ src/test/java/uk/co/autotrader/randomchallenges/naughtsandcrosses/NaughtsAndCrossesTest.java
@@ -1,7 +1,25 @@
 package uk.co.autotrader.randomchallenges.naughtsandcrosses;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 public class NaughtsAndCrossesTest {
 
+    private NaughtsAndCrosses testSubject;
+    private static final String[][] EMPTY_GRID = {{"-", "-", "-"}, {"-", "-", "-"}, {"-", "-", "-"}};
+
+    @Before
+    public void beforeEach() {
+        testSubject = new NaughtsAndCrosses();
+        testSubject.generateGrid();
+
+    }
+
+    @Test
+    public void shouldCreateEmptyGrid() {
+        Assertions.assertThat(testSubject.grid).isEqualTo(EMPTY_GRID);
+    }
 }
