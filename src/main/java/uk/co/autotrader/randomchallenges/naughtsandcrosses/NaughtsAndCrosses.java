15966c51f98da7739e517e66b9f267c5567431f3
fraser-chapman <fraser.chapman@autotrader.co.uk>
2018-12-20 16:31:34 +0000

    added test to check that grid can be displayed and is in correct format and made it pass

--- src/main/java/uk/co/autotrader/randomchallenges/naughtsandcrosses/NaughtsAndCrosses.java
+++ src/main/java/uk/co/autotrader/randomchallenges/naughtsandcrosses/NaughtsAndCrosses.java
@@ -7,4 +7,19 @@ public class NaughtsAndCrosses {
         grid = new String[][]{{"-", "-", "-"}, {"-", "-", "-"}, {"-", "-", "-"}};
     }
 
+    public String displayGrid() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (j == 1) {
+                    stringBuilder.append("|" + grid[i][j] + "|");
+                } else {
+                    stringBuilder.append(grid[i][j]);
+                }
+            }
+            stringBuilder.append("\n");
+        }
+
+        return stringBuilder.toString();
+    }
 }
--- src/test/java/uk/co/autotrader/randomchallenges/naughtsandcrosses/NaughtsAndCrossesTest.java
+++ src/test/java/uk/co/autotrader/randomchallenges/naughtsandcrosses/NaughtsAndCrossesTest.java
@@ -22,4 +22,9 @@ public class NaughtsAndCrossesTest {
     public void shouldCreateEmptyGrid() {
         Assertions.assertThat(testSubject.grid).isEqualTo(EMPTY_GRID);
     }
+
+    @Test
+    public void shouldDisplayGrid() {
+        Assertions.assertThat(testSubject.displayGrid()).isEqualTo("-|-|-\n-|-|-\n-|-|-\n");
+    }
 }
