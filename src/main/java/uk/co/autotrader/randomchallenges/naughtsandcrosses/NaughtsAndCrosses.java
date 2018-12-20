ca1532a9462c2cfa4533b4d73ddb0b0ba1088ad4
fraser-chapman <fraser.chapman@autotrader.co.uk>
2018-12-20 16:33:59 +0000

    added ability to place a counter

--- src/main/java/uk/co/autotrader/randomchallenges/naughtsandcrosses/NaughtsAndCrosses.java
+++ src/main/java/uk/co/autotrader/randomchallenges/naughtsandcrosses/NaughtsAndCrosses.java
@@ -22,4 +22,48 @@ public class NaughtsAndCrosses {
 
         return stringBuilder.toString();
     }
+
+    public void placeCounter(String counterType, String position) {
+        switch (position) {
+            case "top-left":
+                grid[0][0] = counterType.toUpperCase();
+                break;
+
+            case "top-middle":
+                grid[0][1] = counterType.toUpperCase();
+                break;
+
+            case "top-right":
+                grid[0][2] = counterType.toUpperCase();
+                break;
+
+            case "middle-left":
+                grid[1][0] = counterType.toUpperCase();
+                break;
+
+            case "middle-middle":
+                grid[1][1] = counterType.toUpperCase();
+                break;
+
+            case "middle-right":
+                grid[1][2] = counterType.toUpperCase();
+                break;
+
+            case "bottom-left":
+                grid[2][0] = counterType.toUpperCase();
+                break;
+
+            case "bottom-middle":
+                grid[2][1] = counterType.toUpperCase();
+                break;
+
+            case "bottom-right":
+                grid[2][2] = counterType.toUpperCase();
+                break;
+
+            default:
+                System.out.println("invalid input");
+                break;
+        }
+    }
 }
--- src/test/java/uk/co/autotrader/randomchallenges/naughtsandcrosses/NaughtsAndCrossesTest.java
+++ src/test/java/uk/co/autotrader/randomchallenges/naughtsandcrosses/NaughtsAndCrossesTest.java
@@ -27,4 +27,20 @@ public class NaughtsAndCrossesTest {
     public void shouldDisplayGrid() {
         Assertions.assertThat(testSubject.displayGrid()).isEqualTo("-|-|-\n-|-|-\n-|-|-\n");
     }
+
+    @Test
+    public void shouldAddValueToAllLocations() {
+        String expectedResult = "X|X|O\nX|X|X\nO|X|X\n";
+        testSubject.placeCounter("X", "top-left");
+        testSubject.placeCounter("X", "top-middle");
+        testSubject.placeCounter("O", "top-right");
+        testSubject.placeCounter("X", "middle-left");
+        testSubject.placeCounter("X", "middle-middle");
+        testSubject.placeCounter("X", "middle-right");
+        testSubject.placeCounter("O", "bottom-left");
+        testSubject.placeCounter("X", "bottom-middle");
+        testSubject.placeCounter("X", "bottom-right");
+
+        Assertions.assertThat(testSubject.displayGrid()).isEqualTo(expectedResult);
+    }
 }
