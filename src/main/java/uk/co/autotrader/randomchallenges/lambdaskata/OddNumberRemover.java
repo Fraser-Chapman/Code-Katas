b4a1e31494b2f6127c6a3a0b06bb11c3aea7a569
fraser-chapman <fraser.chapman@autotrader.co.uk>
2019-01-04 16:27:04 +0000

    refactored odd number remover and renamed test to be more meaningful

--- src/main/java/uk/co/autotrader/randomchallenges/lambdaskata/OddNumberRemover.java
+++ src/main/java/uk/co/autotrader/randomchallenges/lambdaskata/OddNumberRemover.java
@@ -1,17 +1,15 @@
 package uk.co.autotrader.randomchallenges.lambdaskata;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 class OddNumberRemover {
     List<Integer> removeOddNumbers(int... numbers) {
-        List<Integer> result = new ArrayList<>();
-
-        Arrays.stream(numbers).forEach(number -> result.add(number));
-
-        return result.stream().filter(number -> (number % 2 == 0)).collect(Collectors.toList());
+       return IntStream.of(numbers)
+               .filter(number -> (number % 2 == 0))
+               .boxed()
+               .collect(Collectors.toList());
     }
 }
--- src/test/java/uk/co/autotrader/randomchallenges/lambdaskata/OddNumberRemoverTest.java
+++ src/test/java/uk/co/autotrader/randomchallenges/lambdaskata/OddNumberRemoverTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class OddNumberRemoverTest {
 
     @Test
-    public void testSomeLibraryMethod() {
+    public void removesOddNumbers() {
         OddNumberRemover oddNumberRemover = new OddNumberRemover();
         assertThat(oddNumberRemover.removeOddNumbers(1, 2, 3, 4
         )).containsExactly(2, 4);
