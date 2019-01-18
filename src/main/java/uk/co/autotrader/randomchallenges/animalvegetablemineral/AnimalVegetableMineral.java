04732221943034418a4e8f2ab25c51828113b156
fraser-chapman <fraser.chapman@autotrader.co.uk>
2019-01-18 10:49:46 +0000

    Added functionality to read user input and send it to the askQuestion method placeholder
    Signed-off-by: fraser-chapman <fraser.chapman@autotrader.co.uk>

--- src/main/java/uk/co/autotrader/randomchallenges/animalvegetablemineral/AnimalVegetableMineral.java
+++ src/main/java/uk/co/autotrader/randomchallenges/animalvegetablemineral/AnimalVegetableMineral.java
@@ -1,7 +1,8 @@
 package uk.co.autotrader.randomchallenges.animalvegetablemineral;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
 
 public class AnimalVegetableMineral {
 
@@ -12,29 +13,39 @@ public class AnimalVegetableMineral {
     private static final String QUESTION_5 = "question 5";
     private static final String QUESTION_6 = "question 6";
 
-    public static void printQuestions(List<String> questions) {
-        int count = 0;
+    public static LinkedHashMap<Integer, String> questions;
 
-        for (String question : questions) {
-            count++;
-            System.out.println(count + ": " + question);
-        }
-    }
-
-    private static List<String> buildInitialQuestions() {
-        List<String> questions = new ArrayList<>();
-        questions.add(QUESTION_1);
-        questions.add(QUESTION_2);
-        questions.add(QUESTION_3);
-        questions.add(QUESTION_4);
-        questions.add(QUESTION_5);
-        questions.add(QUESTION_6);
+    private static LinkedHashMap<Integer, String> buildInitialQuestions() {
+        LinkedHashMap questions = new LinkedHashMap();
+        questions.put(1, QUESTION_1);
+        questions.put(2, QUESTION_2);
+        questions.put(3, QUESTION_3);
+        questions.put(4, QUESTION_4);
+        questions.put(5, QUESTION_5);
+        questions.put(6, QUESTION_6);
 
         return questions;
     }
 
+    private static void printQuestions(LinkedHashMap<Integer, String> questions) {
+        System.out.println("What would you like to ask? \n Please enter the corresponding number: ");
+        for (int i = 1; i <= questions.size(); i++) {
+            System.out.println(i + ": " + questions.get(i));
+        }
+    }
+
+    private static Integer getUserQuestion() {
+        Scanner scanner = new Scanner(System.in);
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    private static Boolean askQuestion(int questionNumber){
+        throw new RuntimeException("Feature not yet implemented");
+    }
+
     public static void main(String[] args) {
-        List<String> questions = buildInitialQuestions();
+        questions = buildInitialQuestions();
         printQuestions(questions);
+        askQuestion(getUserQuestion());
     }
 }
